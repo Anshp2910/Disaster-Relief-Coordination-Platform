dac14e8bba8319d5138de4df418792c1c236e9af
Anshp2910 <anshp5388@gmail.com>
2026-06-30 12:00:06 +0530

    chore: remove dead csrfToken/csrfExpires fields from User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -20,13 +20,10 @@ const UserSchema = new mongoose.Schema(
     },
     resetPasswordToken: { type: String, default: undefined },
     resetPasswordExpires: { type: Date, default: undefined },
-    csrfToken: { type: String, default: undefined },
-    csrfExpires: { type: Date, default: undefined },
   },
   { timestamps: true },
 )
 
-UserSchema.index({ csrfExpires: 1 })
 UserSchema.index({ resetPasswordExpires: 1 }, { expireAfterSeconds: 0 })
 UserSchema.index({ email: 1, provider: 1 })
 
