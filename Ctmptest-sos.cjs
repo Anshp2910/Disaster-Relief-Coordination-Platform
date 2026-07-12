8b8f46ed51cb9fbb16e6d2e1ddd12ec6a7574eac
Anshp2910 <anshp5388@gmail.com>
2026-07-12 20:55:05 +0530

    chore: remove stray test file from tracking

--- Ctmptest-sos.cjs
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const { SosAlert } = require('./src/models/SosAlert.js');
-console.log('Model loaded successfully');
-console.log('Schema paths:', Object.keys(SosAlert.schema.paths));
-
